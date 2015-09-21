b12247a4468a2d0b1aa72cf2179ba0a5fc917b69
Andrew Servania <andymasterservania@hotmail.com>
2015-09-21 15:24:44 +0200

    + Minor comment correction

--- SDLFramework/SDLFramework/RabbitWanderingState.cpp
+++ SDLFramework/SDLFramework/RabbitWanderingState.cpp
@@ -19,7 +19,7 @@ void RabbitWanderingState::Enter(Rabbit* rabbit)
 // Execute the code corresponding to the Wandering state for the rabbit
 void RabbitWanderingState::Execute(Rabbit* rabbit)
 {
-	// If the cow is chasing the rabbit and the cow is on the same spot as the cow
+	// If the cow is chasing the rabbit and the cow is on the same spot as the rabbit
 	if (Graph::cow->GetCurrentState() == "Chase Rabbit" &&
 		Graph::cow->getCurrentNode()->id == rabbit->getCurrentNode()->id)
 		rabbit->GetFSM()->ChangeState(RabbitFleeingState::Instance());
